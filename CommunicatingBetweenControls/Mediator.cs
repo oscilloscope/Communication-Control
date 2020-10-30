50b22566e12c2c035b536371a8cadbd383db4ca2
oscilloscope <31646455+oscilloscope@users.noreply.github.com>
2020-10-30 23:15:22 +0300

    First Commit

---
+++ CommunicatingBetweenControls/Mediator.cs
@@ -0,0 +1,34 @@
+﻿using CommunicatingBetweenControls.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunicatingBetweenControls
+{
+    public sealed class Mediator
+    {
+
+        // static members
+        private static readonly Mediator _Instance = new Mediator();
+        private Mediator() { }
+
+        public static Mediator GetInstance() {
+
+            return _Instance;
+        }
+
+        public event EventHandler<JobChangedEventArgs> JobChanged;
+
+        public void OnJobChanged(object sender, Job job) {
+            var jobChangedDelegate = JobChanged as EventHandler<JobChangedEventArgs>;
+            if (jobChangedDelegate != null) {
+                jobChangedDelegate(sender, new JobChangedEventArgs { Job = job });
+            }
+
+        
+        }
+
+    }
+}
